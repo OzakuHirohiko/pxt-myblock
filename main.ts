3d47fcfe16c033b20c8aa437309b5c573494ed7e
OzakuHirohiko <43400583+OzakuHirohiko@users.noreply.github.com>
2019-04-18 15:40:36 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=100 icon="☯" color=#ff6347 block="myBlock"
+namespace myBlock {
+    //% blockId=show_strings block="myBlock %v"
+    export function show(text: string): void {
+        basic.showString(text);
+    }
+}
